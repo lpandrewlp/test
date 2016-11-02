09f0d1e6911aeee4d35fe8fe04c7368304d67433
lpandrewlp <andrewsimanovich@yandex.ru>
2016-11-02 21:41:13 +0300

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    path = require('path');
 
 gulp.task('default', function() {
-    
+    gulp.run('less-boostrap');    
+    gulp.run('less-font-awesome');    
+});
+
+gulp.task('less-boostrap', function() {
+    return gulp.src('./frontend/src/less/bootstrap/bootstrap.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('./frontend/dist/css/'));
+});
+
+gulp.task('less-font-awesome', function() {
+    return gulp.src('./frontend/src/less/font-awesome/font-awesome.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('./frontend/dist/css/'));
 });
